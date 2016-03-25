e7aad9cf6d63b1e06dc594bc2bf0aeee52f6a9b3
hanyinglong <han.yinglong@etaostars.com>
2016-03-25 10:14:26 +0800

    简单工厂方法说明

--- LYZJ.UserLimitMVC.DAL/RepositoryFactory.cs
+++ LYZJ.UserLimitMVC.DAL/RepositoryFactory.cs
@@ -7,16 +7,42 @@ using LYZJ.UserLimitMVC.IDAL;
 
 namespace LYZJ.UserLimitMVC.DAL
 {
-    public static class RepositoryFactory
+    /// <summary>
+    /// 此类不使用，只是为了说明简单工厂模式
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public static class RepositoryFactory<T> where T : class, new()
     {
-        //public static IBaseUserRepository UserInfoRepository
-        //{
-        //    get { return new BaseUserRepository(); }
-        //}
 
-        //public static IBaseRoleRepository RoleRepository
-        //{
-        //    get { return new BaseRoleRepository(); }
-        //}
+        /// <summary>
+        /// 简单工厂模式，最终会将其删除，，demo实例说明
+        /// </summary>
+        /// <param name="respository"></param>
+        /// <returns></returns>
+        public static IBaseRepository<T> CreateRepository(string respository)
+        {
+            IBaseRepository<T> baseRepository = null;
+            switch (respository)
+            {
+                case "baseuser":
+                    baseRepository = (IBaseRepository<T>) new BaseUserRepository();
+                    break;
+                case "baserole":
+                    baseRepository = (IBaseRepository<T>) new BaseRoleRepository();
+                    break;
+            }
+            return baseRepository;
+        }
+
+
+        public static IBaseUserRepository UserInfoRepository
+        {
+            get { return new BaseUserRepository(); }
+        }
+
+        public static IBaseRoleRepository RoleRepository
+        {
+            get { return new BaseRoleRepository(); }
+        }
     }
 }
